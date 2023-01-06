cd7525c1cd9907c80a52268ce0c73fafc50631c1
OleksandrB93 <barabashsan4ik@gmail.com>
2023-01-06 12:04:37 +0200

    fix styled li

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import "./App.css";
-import React from "react";
+import { motion } from 'framer-motion';
+import './App.css';
+import React from 'react';
 
-import { useEffect, lazy } from "react";
-import { Route, Routes } from "react-router-dom";
-import Layout from "components/Layout/Layout";
-import { PrivateRoute } from "components/PrivateRoute";
-import { RestrictedRoute } from "components/RestrictedRoute";
-import { refreshUser } from "redux/auth/operations";
-import { useAuth } from "hooks";
+import { useEffect, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from 'components/Layout/Layout';
+import { PrivateRoute } from 'components/PrivateRoute';
+import { RestrictedRoute } from 'components/RestrictedRoute';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks';
 
-import { useDispatch } from "react-redux";
-import { AppContainer } from "App.styled";
-const HomePage = lazy(() => import("./components/pages/Home/Home"));
-const RegisterPage = lazy(() => import("./components/pages/Register/Register"));
-const LoginPage = lazy(() => import("./components/pages/Login/Login"));
-const ContactsPage = lazy(()=>import("./components/pages/Contacts/ContactsPage"))
-
+import { useDispatch } from 'react-redux';
+import { AppContainer } from 'App.styled';
+const HomePage = lazy(() => import('./components/pages/Home/Home'));
+const RegisterPage = lazy(() => import('./components/pages/Register/Register'));
+const LoginPage = lazy(() => import('./components/pages/Login/Login'));
+const ContactsPage = lazy(() =>
+  import('./components/pages/Contacts/ContactsPage')
+);
 
 export default function App() {
   const dispatch = useDispatch();
   const { isRefreshing } = useAuth();
+
+  const listVAriatns = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+      },
+    },
+    hidden: { opacity: 0, z: -100 },
+  };
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -28,6 +40,7 @@ export default function App() {
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
+    <motion.div variants={listVAriatns} initial="hidden" animate="visible">
       <AppContainer>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -62,6 +75,6 @@ export default function App() {
           </Route>
         </Routes>
       </AppContainer>
+    </motion.div>
   );
 }
-
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,47 @@
-import PropTypes from "prop-types";
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
 
-import React from "react";
-import { DeleteBtn, List, Item, UserInfo, Avatar } from "./ContactLIst.styled";
-import { HiOutlineTrash } from "react-icons/hi";
+import React from 'react';
+import { DeleteBtn, List, Item, UserInfo, Avatar } from './ContactLIst.styled';
+import { HiOutlineTrash } from 'react-icons/hi';
 
-import { useDispatch, useSelector } from "react-redux";
-import { deleteContact } from "redux/contacts/operations";
-import { selectFilteredContacts } from "redux/contacts/selectors";
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contacts/operations';
+import { selectFilteredContacts } from 'redux/contacts/selectors';
 
 export default function ContactList() {
   const contacts = useSelector(selectFilteredContacts);
   const dispatch = useDispatch();
 
+  const listVAriatns = {
+    visible: i => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.1,
+      },
+    }),
+    hidden: { opacity: 0, y: -100 },
+  };
+
   return (
     <>
       <List>
-          {contacts.map(({ id, name, number, avatar }) => {
-            return (
-              <Item key={id}>
+        {contacts.map(({ id, name, number, avatar }, i) => {
+          return (
+            <motion.div
+              key={id}
+              variants={listVAriatns}
+              initial="hidden"
+              animate="visible"
+              custom={i}
+            >
+              <Item>
                 <Avatar
                   src={
                     avatar
                       ? avatar
-                      : "https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
+                      : 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png'
                   }
                   alt="avatar"
                 />
@@ -36,8 +55,9 @@ export default function ContactList() {
                   <HiOutlineTrash size={20} />
                 </DeleteBtn>
               </Item>
-            );
-          })}
+            </motion.div>
+          );
+        })}
       </List>
     </>
   );
